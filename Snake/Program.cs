9fe556ed7ed9026a2fbd2a97900ff3acebaaaa95
Mack McCall <mackmc9924@gmail.com>
2023-08-05 11:04:24 -0600

    created the start of my snake game

---
+++ Snake/Program.cs
@@ -0,0 +1,96 @@
+﻿//Trying to make a snake game
+using Snake;
+
+Console.WindowHeight = 25;
+Console.WindowWidth = 60;
+Console.Title = "Snake";
+Console.CursorVisible = false;
+int x = Console.WindowWidth / 2;
+int y = Console.WindowHeight / 2;
+
+while (true)
+{
+    Console.Clear();
+    Console.SetCursorPosition(x, y);
+    Console.Write("#");
+    ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+
+    while (keyInfo.Key == ConsoleKey.LeftArrow && x > 0)
+    {
+        Console.Clear();
+        Console.SetCursorPosition(x, y);
+        Console.Write("#");
+        x--;
+        Thread.Sleep(100);
+
+        if (Console.KeyAvailable)
+        {
+            ConsoleKeyInfo breakKey = Console.ReadKey(intercept: true);
+
+            if (breakKey.Key != ConsoleKey.LeftArrow)
+            {
+                keyInfo = breakKey;
+            }
+        }
+    }
+    while (keyInfo.Key == ConsoleKey.RightArrow && x < Console.WindowWidth - 1)
+    {
+        Console.Clear();
+        Console.SetCursorPosition(x, y);
+        Console.Write("#");
+        x++;
+        Thread.Sleep(100);
+
+        if (Console.KeyAvailable)
+        {
+            ConsoleKeyInfo breakKey = Console.ReadKey(intercept: true);
+
+            if (breakKey.Key != ConsoleKey.RightArrow)
+            {
+                keyInfo = breakKey;
+            }
+        }
+    }
+    while (keyInfo.Key == ConsoleKey.UpArrow && y > 0)
+    {
+        Console.Clear();
+        Console.SetCursorPosition(x, y);
+        Console.Write("#");
+        y--;
+        Thread.Sleep(150);
+        
+        if (Console.KeyAvailable)
+        {
+            ConsoleKeyInfo breakKey = Console.ReadKey(intercept: true);
+
+            if (breakKey.Key != ConsoleKey.UpArrow)
+            {
+                keyInfo = breakKey;
+            }
+        }
+    }
+    while (keyInfo.Key == ConsoleKey.DownArrow && y < Console.WindowHeight - 1)
+    {
+        Console.Clear();
+        Console.SetCursorPosition(x, y);
+        Console.Write("#");
+        y++;
+        Thread.Sleep(150);
+
+        if (Console.KeyAvailable)
+        {
+            ConsoleKeyInfo breakKey = Console.ReadKey(intercept: true);
+
+            if (breakKey.Key != ConsoleKey.DownArrow)
+            {
+                keyInfo = breakKey;
+            }
+        }
+    }
+
+    if (keyInfo.Key == ConsoleKey.Escape)
+    {
+        return;
+    }
+}
---
+++ Snake/TheSnake.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    public class TheSnake
+    {
+        public void Move(int x, int y)
+        {
+            
+            
+        }
+
+    }
+}
