d7b47e25c70ae9d00a17ec86509c546e10163d3d
Mack McCall <mackmc9924@gmail.com>
2023-12-22 12:58:04 -0700

    metadata

--- Snake/obj/Debug/net7.0/Snake.AssemblyInfo.cs
+++ Snake/obj/Debug/net7.0/Snake.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Snake")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+293e27c75fc6375e9ecf47b8bd456292d2338ba4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Snake")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Snake")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
